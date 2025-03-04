fa6d787e86e7e36f4283e2eefe80e21c23a0ad3e
Francesco Ioli <francesco.ioli@geo.uzh.ch>
2025-03-04 15:25:55 +0000

    bump version 0.1.0 -> 0.1.1

--- src/piperun/__init__.py
+++ src/piperun/__init__.py
@@ -5,7 +5,7 @@ from .shell import Command, OutputCapture, run_command
 from .tasks import DelayedTask
 from .utils.logger import setup_logger
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "Pipeline",
